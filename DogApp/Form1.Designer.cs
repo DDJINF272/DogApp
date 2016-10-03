7fc39207c7d9cda86591b12465ee7584a7166887
DR <DR@DR-PC>
2016-10-03 14:26:36 +0200

    Initial upload

---
+++ DogApp/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace DogApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddNewDog = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAddNewDog
+            // 
+            this.btnAddNewDog.Location = new System.Drawing.Point(28, 22);
+            this.btnAddNewDog.Name = "btnAddNewDog";
+            this.btnAddNewDog.Size = new System.Drawing.Size(191, 23);
+            this.btnAddNewDog.TabIndex = 0;
+            this.btnAddNewDog.Text = "Mangage Dog Species";
+            this.btnAddNewDog.UseVisualStyleBackColor = true;
+            this.btnAddNewDog.Click += new System.EventHandler(this.btnAddNewDog_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Manage Dog Appearance";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 315);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAddNewDog);
+            this.Name = "Form1";
+            this.Text = "Main Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddNewDog;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DogApp/ManageDogAppearance.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace DogApp
+{
+    partial class ManageDogAppearance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxAppearanceID = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAddAppearnace = new System.Windows.Forms.Button();
+            this.btnUpdateAppearance = new System.Windows.Forms.Button();
+            this.btnDeleteAppearance = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(540, 361);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox14);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label9);
+            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.cbxAppearanceID);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(532, 335);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Appearance Details:";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(268, 18);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(175, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "(Select in case of update or delete) ";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(101, 138);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(166, 20);
+            this.textBox4.TabIndex = 17;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(101, 112);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(166, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(101, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(166, 20);
+            this.textBox2.TabIndex = 15;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(166, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 138);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Coat Type ID:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Head Type ID:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Ear Type ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Body Type ID:";
+            // 
+            // cbxAppearanceID
+            // 
+            this.cbxAppearanceID.FormattingEnabled = true;
+            this.cbxAppearanceID.Location = new System.Drawing.Point(107, 18);
+            this.cbxAppearanceID.Name = "cbxAppearanceID";
+            this.cbxAppearanceID.Size = new System.Drawing.Size(144, 21);
+            this.cbxAppearanceID.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select Species ID:";
+            // 
+            // btnAddAppearnace
+            // 
+            this.btnAddAppearnace.Location = new System.Drawing.Point(12, 379);
+            this.btnAddAppearnace.Name = "btnAddAppearnace";
+            this.btnAddAppearnace.Size = new System.Drawing.Size(177, 23);
+            this.btnAddAppearnace.TabIndex = 5;
+            this.btnAddAppearnace.Text = "Add New Appearance";
+            this.btnAddAppearnace.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdateAppearance
+            // 
+            this.btnUpdateAppearance.Location = new System.Drawing.Point(195, 379);
+            this.btnUpdateAppearance.Name = "btnUpdateAppearance";
+            this.btnUpdateAppearance.Size = new System.Drawing.Size(177, 23);
+            this.btnUpdateAppearance.TabIndex = 3;
+            this.btnUpdateAppearance.Text = "Update Selected Appearance";
+            this.btnUpdateAppearance.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteAppearance
+            // 
+            this.btnDeleteAppearance.Location = new System.Drawing.Point(12, 408);
+            this.btnDeleteAppearance.Name = "btnDeleteAppearance";
+            this.btnDeleteAppearance.Size = new System.Drawing.Size(177, 23);
+            this.btnDeleteAppearance.TabIndex = 4;
+            this.btnDeleteAppearance.Text = "Delete Selected Appearance";
+            this.btnDeleteAppearance.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Tail Type ID:";
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(101, 164);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(166, 20);
+            this.textBox14.TabIndex = 21;
+            // 
+            // ManageDogAppearance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 443);
+            this.Controls.Add(this.btnAddAppearnace);
+            this.Controls.Add(this.btnUpdateAppearance);
+            this.Controls.Add(this.btnDeleteAppearance);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "ManageDogAppearance";
+            this.Text = "ManageDogAppearance";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbxAppearanceID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAddAppearnace;
+        private System.Windows.Forms.Button btnUpdateAppearance;
+        private System.Windows.Forms.Button btnDeleteAppearance;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.Label label3;
+    }
+}
